0896545f116e986f546bebbc3f6e66dcaa12f452
Matt Newton <newton10471@gmail.com>
2011-08-19 06:58:56 -0400

    added rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EipcleanGui::Application.config.secret_token = 'd48a7b988bdfb4100b2673c0d29ef360fa32082720a47a2dd4de4d1cd0f5522a1f73fbf909d94e4970e59f61761c93971833e565b076c249fcc9fa921bf04e7d'
